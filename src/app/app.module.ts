d9e93f1f7768dbbae6e4213f5b009ecfdc8bd941
Juan Pablo Mato Hernandez <juanpablomato89@gmail.com>
2024-05-30 11:20:54 -0400

    Start bitly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ShortUrlComponent } from './components/short-url/short-url.component';
+import { SpinnerComponent } from './components/spinner/spinner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ShortUrlComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
