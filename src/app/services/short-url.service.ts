1793b75b51f30e9e16616b400b4c1941418dd0cb
Juan Pablo Mato Hernandez <juanpablomato89@gmail.com>
2024-06-02 12:05:39 -0400

    add http client into service

--- src/app/services/short-url.service.ts
+++ src/app/services/short-url.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,7 @@ import { Injectable } from '@angular/core';
 })
 export class ShortUrlService {
 
-  constructor() { }
+  url = "https://api-ssl.bitly.com/v4/shorten";
+  toke = "f861d12cbca1c76621e1d94b3c0db4270f64284a";
+  constructor(private http: HttpClient) { }
 }
