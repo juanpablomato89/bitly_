05c0fa2b8db1f07a6cdfaa586353ee6650408de1
Juan Pablo Mato Hernandez <juanpablomato89@gmail.com>
2024-06-02 12:36:46 -0400

    Crete method getShortUrl to explain Bearer Token

--- src/app/services/short-url.service.ts
+++ src/app/services/short-url.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +8,22 @@ import { Injectable } from '@angular/core';
 export class ShortUrlService {
 
   url = "https://api-ssl.bitly.com/v4/shorten";
-  toke = "f861d12cbca1c76621e1d94b3c0db4270f64284a";
+  token = "f861d12cbca1c76621e1d94b3c0db4270f64284a";
   constructor(private http: HttpClient) { }
+
+  getShortUrl(nombreUrl: string): Observable<any> {
+
+    const tokenHeaders = new HttpHeaders({
+      Authorization: 'Bearer: ' + this.token
+    })
+    const body = {
+      long_url: nombreUrl
+    }
+
+    return this.http.post(this.url, body,
+      {
+        headers: tokenHeaders
+      })
+  }
+
 }
